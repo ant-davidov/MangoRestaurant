13b22e90b3b03a5aa14541f6813f4974eaf8863a
PC <PC@DESKTOP-SLLCSSD>
2023-02-17 21:43:54 +0300

    add product table

---
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1,1000)]
+        public double Price { get; set; }
+    }
+}
